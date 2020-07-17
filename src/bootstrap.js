1498b82ce9e2ffc9c6743d997b29a75b940ab9e9
SameerRastogi00 <sameer.rastogi1@learner.manipal.edu>
2020-07-17 17:35:45 +0530

    new

--- src/bootstrap.js
+++
@@ -1,9 +0,0 @@
-
-// module.exports=()=>
-// {
-//     const flight=require('./models/flight')
-//     const ticket=require('./models/ticket')
-//     const user =require('./models/user')
-//     user.hasMany(ticket,{as:'tickets',foreignKey:'userId'})
-//     ticket.belongsTo(user,{as:"user",foreignKey:'userId'})
-// }
